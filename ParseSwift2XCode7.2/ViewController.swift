0626cfa7919fe1ebecb0fba7e70731f277c781a6
LeandroLee <LeandroLee@leandrolees-mac.local>
2016-01-22 13:27:57 -0200

    Initial Commit

---
+++ ParseSwift2XCode7.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseSwift2XCode7.2
+//
+//  Created by LeandroLee on 1/22/16.
+//  Copyright © 2016 goldhandsapp4u. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
